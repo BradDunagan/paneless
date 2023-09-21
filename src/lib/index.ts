678dfa65b30926d0bbdeae9327f09883ceb53f41
BradDunagan <brad.dunagan@gmail.com>
2023-09-21 13:47:48 -0700

    export uLabel, uPanel, uTree

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,7 +5,10 @@ import { getFrameId }		from '$lib/paneless/frame-id';
 import { getPaneId }		from '$lib/paneless/pane-id';
 import type { Item }	    from '$lib/paneless/interfaces';
 import { uc } 				from '$lib/paneless/udui/udui-common';
+import { uLabel } 			from '$lib/paneless/udui/udui-label-b';
 import { uList } 			from '$lib/paneless/udui/udui-list-b';
+import { uPanel }			from '$lib/paneless/udui/udui-panel-f';
+import { uTree }			from '$lib/paneless/udui/udui-tree-a';
 
 export default AppFrame;
 export type { Item };
@@ -14,4 +17,7 @@ export { cmn,
          getFrameId, 
          getPaneId,
 		 uc,
-		 uList };
+		 uLabel,
+		 uList,
+		 uPanel,
+		 uTree };
